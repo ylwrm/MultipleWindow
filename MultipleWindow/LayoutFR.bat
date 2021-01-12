322bc5041365fe40e3296b5598d58c1b33276cb6
jerry <398265072@qq.com>
2021-01-13 03:02:09 +0800

    demo evn

---
+++ MultipleWindow/LayoutFR.bat
@@ -0,0 +1,3 @@
+call "%~dp0env.bat"
+cd work
+..\MultipleWindow.exe ..\LayoutFR.xml
--- MultipleWindow/env.bat
+++ MultipleWindow/env.bat
@@ -1,4 +1,11 @@
 set "cd=%~dp0"
+
+set "ExcelExe=C:\Program Files\Microsoft Office\Office15\EXCEL.EXE"
+
 set "zw3dExe=D:\Program Files\ZWSOFT\ZW3D 2021\zw3d.exe"
 set "ZWSimExe=D:\Program Files\ZWSOFT\ZWSim Structural 2021\ZWSim.exe"
- 
+
+
+set "CreoExe=C:\Program Files\PTC\Creo 2.0\Parametric\bin\parametric.exe"
+set "AbaqusExe=C:\Program Files\SIMULIA\ABAQUS\6.14-2\code\bin\abq6142.exe"
+set "CFDPostExe=C:\Program Files\ANSYS Inc\v202\CFD-Post\bin\cfdpost.exe"
