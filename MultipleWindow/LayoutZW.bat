b8c1f0d45e61bbf09e441f75613fb5ca5221e603
jerry <398265072@qq.com>
2021-01-13 02:20:46 +0800

    zw

---
+++ MultipleWindow/LayoutZW.bat
@@ -0,0 +1,3 @@
+call "%~dp0env.bat"
+cd work
+..\MultipleWindow.exe ..\LayoutZW.xml
---
+++ MultipleWindow/env.bat
@@ -0,0 +1,4 @@
+set "cd=%~dp0"
+set "zw3dExe=D:\Program Files\ZWSOFT\ZW3D 2021\zw3d.exe"
+set "ZWSimExe=D:\Program Files\ZWSOFT\ZWSim Structural 2021\ZWSim.exe"
+ 
